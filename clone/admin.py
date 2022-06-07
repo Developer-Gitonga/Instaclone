5d176a0bba62a504060d2591c988fff7ab52b40f
Dev Git <95219841+Developer-Gitonga@users.noreply.github.com>
2022-06-07 21:41:22 +0300

    Update admin.py

--- clone/admin.py
+++ clone/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Image, Comment,Like, Profile
-# Register your models here.
 
+# Register your models here.
 admin.site.register(Image)
 admin.site.register(Comment)
 admin.site.register(Like)
